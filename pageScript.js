b4a9b26a11685b4b8fd21f160b9e05388710776d
yuzzzz <121236723+NekoSenpai06@users.noreply.github.com>
2024-06-14 01:18:18 +0800

    Add files via upload

---
+++ pageScript.js
@@ -0,0 +1,159 @@
+function letter1() {
+     location.href = "letters/001.html";
+}
+
+function letter2() {
+     location.href = "letters/002.html";
+}
+
+function letter3() {
+     location.href = "letters/003.html";
+}
+
+function letter4() {
+     location.href = "letters/004.html";
+}
+
+function letter5() {
+     location.href = "letters/005.html";
+}
+
+function letter6() {
+     location.href = "letters/006.html";
+}
+
+function letter7() {
+     location.href = "letters/007.html";
+}
+
+function letter8() {
+     location.href = "letters/008.html";
+}
+
+function letter9() {
+     location.href = "letters/009.html";
+}
+
+function letter10() {
+     location.href = "letters/010.html";
+}
+
+function letter11() {
+     location.href = "letters/011.html";
+}
+
+function letter12() {
+     location.href = "letters/012.html";
+}
+
+function letter13() {
+     location.href = "letters/013.html";
+}
+
+function letter14() {
+     location.href = "letters/014.html";
+}
+
+function letter15() {
+     location.href = "letters/015.html";
+}
+
+function letter16() {
+     location.href = "letters/016.html";
+}
+
+function letter17() {
+     location.href = "letters/017.html";
+}
+
+function letter18() {
+     location.href = "letters/018.html";
+}
+
+function letter19() {
+     location.href = "letters/019.html";
+}
+
+function letter20() {
+     location.href = "letters/020.html";
+}
+
+function letter21() {
+     location.href = "letters/021.html"
+}
+
+function letter22() {
+     location.href = "letters/022.html"
+}
+
+function letter23() {
+     location.href = "letters/023.html"
+}
+
+function letter24() {
+     location.href = "letters/024.html"
+}
+
+function letter25() {
+     location.href = "letters/025.html"
+}
+
+function letter26() {
+     location.href = "letters/026.html";
+}
+
+function letter27() {
+     location.href = "letters/027.html";
+}
+
+function letter28() {
+     location.href = "letters/028.html";
+}
+
+function letter29() {
+     location.href = "letters/029.html";
+}
+
+function letter30() {
+     location.href = "letters/030.html";
+}
+
+function letter31() {
+     location.href = "letters/031.html";
+}
+
+function letter32() {
+     location.href = "letters/032.html";
+}
+
+function letter33() {
+     location.href = "letters/033.html";
+}
+
+function letter34() {
+     location.href = "letters/034.html";
+}
+
+function letter35() {
+     location.href = "letters/035.html";
+}
+
+function letter36() {
+     location.href = "letters/036.html";
+}
+
+function letter37() {
+     location.href = "letters/037.html";
+}
+
+function letter38() {
+     location.href = "letters/038.html";
+}
+
+function letter39() {
+     location.href = "letters/039.html";
+}
+
+function letter40() {
+     location.href = "letters/040.html";
+}
---
+++ script.js
@@ -0,0 +1,11 @@
+var password = document.querySelector("input");
+
+function paswrd() {
+     if(password.value === "us0607") {
+          location.href = "page.html";
+     }
+          
+     else {
+          alert("✘");
+     }
+}
